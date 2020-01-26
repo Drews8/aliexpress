b9b38ddc94738019b0c08878a305c32f945b8271
Drews8 <prokhorenko.drew@gmail.com>
2020-01-26 14:56:33 +0200

    add category filter and fetch data from db

--- script/script.js
+++ script/script.js
@@ -5,6 +5,9 @@ document.addEventListener('DOMContentLoaded', () => {
     const wishlistBtn = document.querySelector('#wishlist');
     const goodsWrapper = document.querySelector('.goods-wrapper');
     const cart = document.querySelector('.cart');
+    const category = document.querySelector('.category');
+
+
 
     const createCardGoods = (id, title, price, img) => {
         const card = document.createElement('div');
@@ -12,7 +15,7 @@ document.addEventListener('DOMContentLoaded', () => {
         card.innerHTML = `<div class="card">
                                 <div class="card-img-wrapper">
                                     <img class="card-img-top" src="${img}" alt="">
-                                    <button class="card-add-wishlist" data-goods-id="${id}"></button>
+                                     <button class="card-add-wishlist" data-goods-id="${id}"></button>
                                 </div>
                                 <div class="card-body justify-content-between">
                                     <a href="#" class="card-title">${title}</a>
@@ -23,41 +26,63 @@ document.addEventListener('DOMContentLoaded', () => {
                                     </div>
                                 </div>
                             </div>`;
-        console.log(card);
         return card;
     };
 
-    const openCart = () => {
+    const openCart = event => {
+        event.preventDefault();
         cart.style.display = 'flex';
-
-        /*const escapeBtnClose = function (event) {
-            if(event.key === 'Escape'){
-                document.removeEventListener('keydown', escapeBtnClose)
-                cart.style.display = 'none';
-            }
-        };
-        document.addEventListener('keydown', escapeBtnClose);*/
-
-
-        /*cart.querySelector('a').addEventListener('click', (event) => {
-            console.log("complete");
-            event.preventDefault();
-        });*/
+        document.addEventListener('keyup', closeCart);
     };
+
     const closeCart = (event) => {
         const target = event.target;
 
-        if (target.classList.contains('cart') || target.classList.contains('cart-close')) {
+        if (target.classList.contains('cart') ||
+            target.classList.contains('cart-close') ||
+            event.key === 'Escape') {
             cart.style.display = 'none';
+            document.removeEventListener('keyup', closeCart);
         }
-
     };
 
-    goodsWrapper.append(createCardGoods(1, 'Darts', 2000, 'img/temp/Archer.jpg'));
-    goodsWrapper.append(createCardGoods(2, 'Flamingo', 3000, 'img/temp/Flamingo.jpg'));
-    goodsWrapper.append(createCardGoods(3, 'Socks', 300, 'img/temp/Socks.jpg'));
+    const renderCard = items => {
+        goodsWrapper.textContent = '';
+        items.forEach((item) => {
+            const { id, title, price, imgMin} = item;
+            goodsWrapper.append(createCardGoods(id, title, price, imgMin));
+        });
+    };
+
+    const randomSort = items => {
+
+        return items.sort(() => Math.random() - 0.5);
+    }
+
+    const getGoods = (handler, filter) => {
+        fetch('db/db.json')
+            .then(response => response.json())
+            .then(filter)
+            .then(handler);
+    };
+
+    const choiceCategory = event => {
+        event.preventDefault();
+        const target = event.target;
+
+        if (target.classList.contains('category-item')) {
+            const categoryName = target.dataset.category;
+            getGoods(renderCard, goods => {
+                    return goods.filter(item => item.category.includes(categoryName));
+            });
+        }
+        
+    };
 
 
     cartBtn.addEventListener('click', openCart);
     cart.addEventListener('click', closeCart);
+    category.addEventListener('click', choiceCategory);
+
+    getGoods(renderCard, randomSort);
 });
